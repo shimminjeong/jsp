668cede4cc6b3ee0e190fa668be6fce124c4c43b
simminjeong <pooh5045@naver.com>
2023-06-20 15:23:34 +0900

    8-2 데이터공유 : ServletContext

--- src/main/java/com/jsplec/ex2/InParameterEx2.java
+++ src/main/java/com/jsplec/ex2/InParameterEx2.java
@@ -1,25 +1,22 @@
 package com.jsplec.ex2;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebInitParam;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
- * Servlet implementation class InParameterEx
+ * Servlet implementation class InParameterEx2
  */
-//@WebServlet(urlPatterns={"/InParameterEx"},initParams= {@WebInitParam(name="dbId",value="abcde"),@WebInitParam(name="dbPwd",value="dbPwd")})
-public class InParameterEx extends HttpServlet {
+public class InParameterEx2 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public InParameterEx() {
+    public InParameterEx2() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -30,18 +27,20 @@ public class InParameterEx extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-		String dbId=getInitParameter("dbId");
-		String dbPwd=getInitParameter("dbPwd");
-		String dbSid=getInitParameter("dbSid");
-		String testId=getInitParameter("testId");
 		
-		PrintWriter pw = response.getWriter();
+		String dbId=getServletContext().getInitParameter("dbId");
+        String dbPwd=getServletContext().getInitParameter("dbPwd");
+        String dbSid=getServletContext().getInitParameter("dbSid");
+        String testId=getServletContext().getInitParameter("testId");
+        response.setContentType("text/html; charset=UTF-8");
+        PrintWriter pw = response.getWriter();
+
         
         pw.println("<html>");
         pw.println("<head>");
         pw.println("</head>");
         pw.println("<body>");
-        pw.println("<h1>Hello World!!</h1>");
+        pw.println("<h1> contextparam servlet 페이지 입니다.!!</h1>");
         pw.println("<p>dbId: " + dbId + "</p>");
         pw.println("<p>dbPwd: " + dbPwd + "</p>");
         pw.println("<p>dbSid: " + dbSid + "</p>");
@@ -50,7 +49,6 @@ public class InParameterEx extends HttpServlet {
         pw.println("</html>");
         
         pw.close();
-        
 	}
 
 	/**
